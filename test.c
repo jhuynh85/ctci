e2496b1480fc36bacb8cecb6d1b87d2591d984a2
Joseph Huynh <joe.huynh@gmail.com>
2016-08-01 21:42:02 -0700

    1.4

--- test.c
+++ test.c
@@ -16,9 +16,32 @@ void reverse (char* str){
     }
 }
 
+/* 1.4 Function that replaces whitespace with %20 */
+void replace (char* str){
+	int count=0;
+	int i;
+	
+	for (i=0; i<strlen(str); i++){
+		if (str[i]==' ') count++;
+	}
+	
+	int finalLength = strlen(str)+(count*2);
+	str[finalLength--]='\0';
+	
+	for (i=strlen(str)-1; i>=0; i--){
+		if(str[i]==' '){
+			str[finalLength--]='0';
+			str[finalLength--]='2';
+			str[finalLength--]='%';
+		} else {
+			str[finalLength--]=str[i];
+		}
+	}
+}
+
 int main (int argc, char **argv){
-    char* str = argv[1];
+    char str[30] = "Mr John Smith   ";
     
-    reverse(str);
+  	replace(str);
     printf("%s\n", str);
 }
